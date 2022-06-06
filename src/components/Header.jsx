2d1110ca180a22ae439486fc58396acefd7e56ec
SK SAHIL <sksahil.contact@gmail.com>
2022-06-06 23:45:34 +0530

    Add links to home, listing and login page using useNavigate of react router dom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import './Header.css';
+
 function Header() {
+  const navigate = useNavigate();
   return (
     <AppBar position='static' className='AppBar'>
       <Toolbar>
         <div className='leftNav'>
-          <Button color='inherit'>
+          <Button color='inherit' onClick={() => navigate('/')}>
             <Typography variant='h4'>Dream Location</Typography>
           </Button>
         </div>
         <div>
-          <Button color='inherit' className='listingsBtn'>
+          <Button
+            color='inherit'
+            className='listingsBtn'
+            onClick={() => navigate('/listings')}
+          >
             <Typography variant='h6'>Listings</Typography>
           </Button>
           <Button color='inherit' className='agenciesBtn'>
@@ -19,7 +26,9 @@ function Header() {
         </div>
         <div className='rightNav'>
           <Button className='propertyBtn'>Add Property</Button>
-          <Button className='loginBtn'>Login</Button>
+          <Button className='loginBtn' onClick={() => navigate('/login')}>
+            Login
+          </Button>
         </div>
       </Toolbar>
     </AppBar>
